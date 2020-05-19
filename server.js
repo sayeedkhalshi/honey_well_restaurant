af7fdc28fb3d9633a75186e290e1597503843351
psayeed1990 <sayeedmondal1412@gmail.com>
2020-05-19 12:50:26 +0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+
+//load routes
+const index = require("./api/routes/index");
+const user = require("./api/routes/user");
+const reservations = require("./api/routes/reservations");
+
+//db config
+db = require("./config/keys").mongoURI;
+
+//connect mongo
+mongoose
+    .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => console.log("mongoose connected"))
+    .catch((err) => console.log(err));
+
+//set static
+app.use(express.static(__dirname + "/public"));
+
+//set view
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+//routes
+app.use("/", index);
+app.use("/user", user);
+app.use("/reservations", reservations);
+
+//port
+const PORT = process.env.PORT || 5000;
+
+//start server
+app.listen(PORT, console.log(`server started at ${PORT}`));
